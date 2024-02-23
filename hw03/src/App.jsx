72b0f4fd30f2c2bd99a7fa2014def1146f219b15
Alexandr <shlyapnikoff2012@gmail.com>
2024-02-23 22:02:18 +0500

    dark theme added

--- hw03/src/App.jsx
+++ hw03/src/App.jsx
@@ -1,10 +1,20 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import TemperatureConverter from './components/TemperatureConverter';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
 
 function App() {
 	return (
-		<>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
 			<TemperatureConverter />
-		</>
+			<main>This app is using the dark mode</main>
+		</ThemeProvider>
 	);
 }
 
